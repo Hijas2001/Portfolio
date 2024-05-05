7b01c5bbcb43f6509e1dd4e9741caa6792a3ba8c
Hijas k <hijaskallipparamban@gmail.com>
2024-05-05 07:37:36 +0530

    Hero file created

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+// import React from 'react'
+import logoicon from '../assets/HKprofile.jpg';
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitterSquare } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className=" mb-20 flex items-center justify-between py-2">
+      <div className="flex flex-shrink-0 items-center">
+        <img className='w-16 rounded-lg mx-2' src={logoicon} alt="logo" />
+      </div>
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <FaLinkedin></FaLinkedin>
+        <FaGithub></FaGithub>
+        <FaInstagram></FaInstagram>
+        <FaTwitterSquare></FaTwitterSquare>
+      </div>
+    </nav>
+
   )
+
 }
 
 export default Navbar
